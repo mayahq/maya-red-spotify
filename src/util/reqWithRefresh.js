a829b5de9ad25aceeb0c341f606a846034a697e9
Dushyant Yadav <dushyant9309@gmail.com>
2022-03-25 17:05:12 +0530

    Use new tokens API to handle token refresh

--- src/util/reqWithRefresh.js
+++ src/util/reqWithRefresh.js
@@ -1,13 +1,22 @@
 const axios = require('axios')
 const refresh = require('./refresh')
 
-async function makeRequestWithRefresh(node, request) {
+async function makeRequestWithRefresh(node, request, { force = false } = {}) {
     try {
+        // // Testing token refresh
+        // const e = new Error('')
+        // e.response = { status: 401 }
+        // throw e
+
         const response = await axios(request)
         return response
     } catch (e) {
         if (e.response && parseInt(e.response.status) === 401) {
-            const { access_token } = await refresh(node)
+            const start = Date.now()
+            const { access_token, fromCache } = await refresh(node, { force })
+            const end = Date.now()
+
+            console.log(`Token refresh took ${end-start}ms`)
             if (!access_token) {
                 const err = new Error('Unable to refresh tokens')
                 err.name = 'TOKEN_REFRESH_FAILED'
@@ -15,8 +24,22 @@ async function makeRequestWithRefresh(node, request) {
             }
 
             request.headers.Authorization = `Bearer ${access_token}`
-            const response = await axios(request)
-            return response
+            try {
+                const response = await axios(request)
+                return response
+            } catch (e) {
+                // If the request fails even after refresh but the new token was fetched from cache,
+                // force a refresh of the cloud token and attempt the request with that.
+                if (!fromCache) throw e
+                if (e.response && parseInt(e.response.status) === 401) {
+                    const { access_token } = await refresh(node, { force: true })
+                    request.headers.Authorization = `Bearer ${access_token}`
+                    const response = await axios(request)
+                    return response
+                } else {
+                    throw e
+                }
+            }
         } else {
             throw e
         }
